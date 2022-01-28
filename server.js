1d1cc106bfd3499319f29b4c3bdd9854f2da2316
Albertine <albertine.map@gmail.com>
2022-01-28 17:31:38 -0500

    download file

---
+++ server.js
@@ -0,0 +1,27 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+let fs = require('fs')
+let file = 'demo.mp4'
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/get_file', (req, res) => {
+    fs.stat(file, (err, stat) => {
+        let total = stat.size
+        let read = fs.createReadStream(file)
+        res.writeHead(200, {
+            'Content-Length': total,
+            'Content-Type': 'video/mp4',
+            "Content-Disposition": 'attachment; filename="demo.mp4"',
+            }); 
+        read.pipe(res) 
+    })
+})
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
