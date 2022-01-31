52fda0f440ab0ac7b540ba6e86484a2a284600bc
Albertine <albertine.map@gmail.com>
2022-01-31 12:00:08 -0500

    create database

--- server.js
+++ server.js
@@ -9,12 +9,41 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root"
+  password: "",
+//   database:"nserver_test"
 });
 
+//connection
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  //database
+  con.query("drop database if exists nserver_test", function (err, result) {
+    if (err) throw err;
+    console.log("Database nserver_test delete");
+  });
+
+  con.query("CREATE DATABASE nserver_test", function (err, result) {
+    if (err) throw err;
+    console.log("Database nserver_test created");
+  });
+  //table
+  var sql = "CREATE TABLE nserver_test.customers (name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+  //insert
+  var sql = "INSERT INTO nserver_test.customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+  //select
+  con.query("SELECT * FROM nserver_test.customers", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
 });
 
 app.get('/', (req, res) => {
