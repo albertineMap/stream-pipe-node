f6191b1eaca74f4e3dd3826db1e63087013c8bb7
andee777 <andee.k@hotmail.com>
2022-02-01 15:48:24 -0500

    string concat

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const spawn = require('child_process').spawn
 
 var mysql = require('mysql');
 
-
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -23,27 +22,27 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  //database
-  con.query("drop database if exists nserver_test", function (err, result) {
-    if (err) throw err;
-    console.log("Database nserver_test delete");
-  });
-  con.query("CREATE DATABASE nserver_test", function (err, result) {
-    if (err) throw err;
-    console.log("Database nserver_test created");
-  });
-  //table
-  var sql = "CREATE TABLE nserver_test.customers (name VARCHAR(255), address VARCHAR(255))";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Table created");
-  });
-  //insert
-  var sql = "INSERT INTO nserver_test.customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
+  // //database
+  // con.query("drop database if exists nserver_test", function (err, result) {
+  //   if (err) throw err;
+  //   console.log("Database nserver_test delete");
+  // });
+  // con.query("CREATE DATABASE nserver_test", function (err, result) {
+  //   if (err) throw err;
+  //   console.log("Database nserver_test created");
+  // });
+  // //table
+  // var sql = "CREATE TABLE nserver_test.customers (name VARCHAR(255), address VARCHAR(255))";
+  // con.query(sql, function (err, result) {
+  //   if (err) throw err;
+  //   console.log("Table created");
+  // });
+  // //insert
+  // var sql = "INSERT INTO nserver_test.customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  // con.query(sql, function (err, result) {
+  //   if (err) throw err;
+  //   console.log("1 record inserted");
+  // });
   //select
   con.query("SELECT * FROM nserver_test.customers", function (err, result, fields) {
     if (err) throw err;
@@ -65,19 +64,24 @@ app.get('/get_file', async (req, res) => {
       },
       //dumpToFile: './dump.sql.gz',
   });
-  //console.log(result);
+  // console.log(result);
   //let read = fs.createReadStream(Object.values(result));
-  const read = new Readable();
 
-  read.push(JSON.stringify(result));
+  const read = new Readable();
+  let dumpFinal =  "";
+  dumpFinal += result.dump.schema;
+  dumpFinal += "\n\n";
+  dumpFinal += result.dump.data;
+
+  // console.log("\n\n" + dumpFinal);
+  
+  read.push(dumpFinal);
   read.push(null);
-  //read.pipe(process.stdout)
-  //console.log(read);
   
   res.writeHead(200, {
       'Content-Type': 'application/javascript',
       'Content-Encoding': 'gz',
-      "Content-Disposition": 'attachment; filename="nserverdb.gz"',
+      "Content-Disposition": 'attachment; filename="nserverdb.sql.gz"',
       }); 
       
   read.pipe(res) 
